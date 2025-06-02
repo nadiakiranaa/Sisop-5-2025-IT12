42d3372ac06d2e5122bd9dbeeab74a0e482d1714
KhosySyehab <syehabkhosyi@gmail.com>
2025-06-03 03:22:36 +0700

    edit std_lib.c

--- src/std_lib.c
+++ src/std_lib.c
@@ -1,39 +1,111 @@
 #include "std_lib.h"
 
-int div(int a, int b)
-{
-  //TODO: Implementasi pembagian
-  //NOTE: HARUS BISA ANGKA NEGATIF
+int div(int a, int b) {
+  bool neg = false;
+  if (a < 0) {
+    a = -a;
+    neg = !neg;
+  }
+  if (b < 0) {
+    b = -b;
+    neg = !neg;
+  }
+
+  int res = 0;
+  while (a >= b) {
+    a -= b;
+    res++;
+  }
+
+  return neg ? -res : res;
 }
 
-int mod(int a, int b)
-{
-  //TODO: Implementasi modulus
+int mod(int a, int b) {
+  bool neg = false;
+  if (a < 0) {
+    a = -a;
+    neg = true;
+  }
+  if (b < 0) b = -b;
+
+  while (a >= b) {
+    a -= b;
+  }
+
+  return neg ? -a : a;
 }
 
-bool strcmp(char *str1, char *str2)
-{
-  //TODO: Implementasi perbandingan string
+bool strcmp(char *str1, char *str2) {
+  int i = 0;
+  while (str1[i] != '\0' && str2[i] != '\0') {
+    if (str1[i] != str2[i]) return false;
+    i++;
+  }
+  return str1[i] == str2[i];
 }
 
-void strcpy(char *dst, char *src)
-{
-  //TODO: Implementasi penyalinan string
+void strcpy(char *dst, char *src) {
+  int i = 0;
+  while (src[i] != '\0') {
+    dst[i] = src[i];
+    i++;
+  }
+  dst[i] = '\0';
 }
 
-void clear(byte *buf, unsigned int size)
-{
-  //TODO: Implementasi pembersihan buffer
+void clear(byte *buf, unsigned int size) {
+  for (int i = 0; i < size; i++) {
+    buf[i] = 0;
+  }
 }
 
-void atoi(char *str, int *num)
-{
-  //TODO: Implementasi konversi string ke integer
-  //NOTE: HARUS BISA ANGKA NEGATIF
+void atoi(char *str, int *num) {
+  int i = 0;
+  int res = 0;
+  bool neg = false;
+
+  if (str[i] == '-') {
+    neg = true;
+    i++;
+  }
+
+  while (str[i] >= '0' && str[i] <= '9') {
+    res = res * 10 + (str[i] - '0');
+    i++;
+  }
+
+  *num = neg ? -res : res;
 }
 
-void itoa(int num, char *str)
-{
-  //TODO: Implementasi konversi integer ke string
-  //NOTE: HARUS BISA ANGKA NEGATIF
+void itoa(int num, char *str) {
+  int i = 0;
+  bool neg = false;
+  char temp[16];
+
+  if (num == 0) {
+    str[0] = '0';
+    str[1] = '\0';
+    return;
+  }
+
+  if (num < 0) {
+    neg = true;
+    num = -num;
+  }
+
+  while (num > 0) {
+    temp[i++] = (num % 10) + '0';
+    num = num / 10;
+  }
+
+  if (neg) {
+    temp[i++] = '-';
+  }
+
+  // Reverse
+  int j = 0;
+  while (i > 0) {
+    str[j++] = temp[--i];
+  }
+  str[j] = '\0';
 }
