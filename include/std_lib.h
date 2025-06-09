5e997004a41068d2635aa20fbe93ee53ec138eb7
KhosySyehab <syehabkhosyi@gmail.com>
2025-06-09 11:33:56 +0700

    Update std_lib.h

--- include/std_lib.h
+++ include/std_lib.h
@@ -6,11 +6,11 @@
 int div(int a, int b);
 int mod(int a, int b);
 
-bool strcmp(char *str1, char *str2);
+int strcompare(char *str1, char *str2);
 void strcpy(char *dst, char *src);
 void clear(byte *buf, unsigned int size);
 
-void atoi(char *str, int *num);
+int strtoint(char *str, int *num);
 void itoa(int num, char *str);
 
 #endif // __STD_LIB_H__
